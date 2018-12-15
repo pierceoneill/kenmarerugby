a341af653a3269f8dc8dd1c346a81d7fdc985d70
Pierce ONeill <pierceoneillky@gmail.com>
2018-12-15 23:20:30 +0000

    favicons

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 def index(request):
+    posts = Post.objects.order_by('-published_date').filter()[:3]
+
+    context = {
+        'posts': posts,
+    }
   
-    return render(request, 'index.html',)
+    return render(request, 'index.html', context)
