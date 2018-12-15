f7fab85130b65f1da4278f771bb104f5702685ec
Pierce ONeill <pierceoneillky@gmail.com>
2018-12-15 17:48:26 +0000

    update postgres

--- kenmarerugby/settings.py
+++ kenmarerugby/settings.py
@@ -90,10 +90,17 @@ WSGI_APPLICATION = 'kenmarerugby.wsgi.application'
 #     }
 # }
 
-
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
