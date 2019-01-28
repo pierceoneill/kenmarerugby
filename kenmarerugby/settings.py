646fe9255b463c29051460f9ad66f205ed427752
Pierce ONeill <pierceoneillky@gmail.com>
2019-01-28 14:15:15 +0000

    settings

--- kenmarerugby/settings.py
+++ kenmarerugby/settings.py
@@ -149,13 +149,13 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+    os.path.join(BASE_DIR, "static"),
+    )
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
@@ -165,5 +165,3 @@ MEDIA_URL = '/media/'
 
 
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
